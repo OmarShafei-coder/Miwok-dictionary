cf2e275e7c3136353bf43c85226b3f1f15828f61
Omar Shafei <omarshafhy.os@gmail.com>
2019-07-09 19:56:27 +0200

    using ArrayList instead of the normal an array

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,9 +10,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        //creating an arrayList for numbers in English
+        ArrayList<String> words = new ArrayList<String>();
 
-        String[] englishWords = {"one", "two", "three", "four", "five",
-                                 "six", "seven", "eight", "nine", "ten"};
-
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
     }
 }
