ac0da47670fdd0497097e332f709febdd1fce9ea
Omar Shafei <omarshafhy.os@gmail.com>
2019-07-09 15:05:52 +0200

    Declaring an array of english words

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] englishWords = {"one", "two", "three", "four", "five",
+                                 "six", "seven", "eight", "nine", "ten"};
+
     }
 }
