2a4a85c20002c3e1c897e681642eddb738f79b5d
Omar Shafei <omarshafhy.os@gmail.com>
2019-07-09 22:12:33 +0200

    using ArrayList with for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
@@ -23,5 +26,15 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        int index;
+        for(index=0; index<words.size(); index++)
+        {
+            TextView wordView = new TextView(this);
+            rootView.addView(wordView);
+            wordView.setText(words.get(index));
+        }
     }
 }
