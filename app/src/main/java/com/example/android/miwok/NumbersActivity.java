754377f358bbe22814ad93899aae8097cecba6d9
Omar Shafei <omarshafhy.os@gmail.com>
2019-07-18 13:18:14 +0200

    Experementing an audio file for all categories, and adding all Audio files

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
